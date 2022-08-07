575d2f9896fb171292c966a80151cfca7b98bb16
Steven Chen <junyich@uw.edu>
2022-08-06 23:26:24 -0700

    Finished about us page, implement the nav thing and set up nav in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NavBar from './Navigation';
+import AboutUs from './AboutUs';
 
 function App() {
-    return (
-        <NavBar />
+    
+    const [selectFeature, setSelectFeature] = useState("main");
+
+    const applySelectFeature = (featureStr) => {
+        setSelectFeature(featureStr)
+    }
+    console.log(selectFeature);
+
+
+    let renderContent = (
+        <div>
+            <NavBar applySelect={applySelectFeature} />
+
+            {/* rest of main page content */}
+            
+        </div>
     )
+
+    if (selectFeature === "topic") {
+        // only render topic component with navbar
+        renderContent = (
+            <div>
+                <NavBar applySelect={applySelectFeature} />
+
+                {/* topic component here */}
+            
+            </div>
+        )
+    } else if (selectFeature === "survey") {
+        // do shit
+        renderContent = (
+            <div>
+                <NavBar applySelect={applySelectFeature} />
+
+                {/* survey component here */}
+            
+            </div>
+        )
+    } else if (selectFeature === "lifestyle") {
+        // do shit
+        renderContent = (
+            <div>
+                <NavBar applySelect={applySelectFeature} />
+
+                {/* lifestyle component here */}
+            
+            </div>
+        )
+    } else if (selectFeature === "book") {
+        // do shit
+        renderContent = (
+            <div>
+                <NavBar applySelect={applySelectFeature} />
+
+                {/* book component here */}
+            
+            </div>
+        )
+    } else if (selectFeature === "aboutus") {
+        renderContent = (
+            <div>
+                <NavBar applySelect={applySelectFeature} />
+
+                <AboutUs />
+            
+            </div>
+        )
+    } else { // if back to main
+        <div>
+            <NavBar applySelect={applySelectFeature} />
+
+            {/* rest of main page content */}
+            
+        </div>
+    }
+
+    return renderContent;
 }
 
 export default App;
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,25 +1,38 @@
 import React from 'react';
 
 export default function NavBar(props) {
+
+
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        props.applySelect(event.currentTarget.name)
+    }
+
+    // console.log(selectFeature);
+
+
     return (
         <div className="flex-container">
             <header className="flex-header pb-3">
                 <nav>
                     <div className="icon">
                         
-                        <img className="header-item2" src="img/imentalPSed.png" alt="icon of iMental" />
-                        
+                        <a href='#/' onClick={handleClick} name="main">
+                            <img className="header-item2" src="img/imentalPSed.png" alt="icon of iMental" />
+                        </a>
+
                     </div>
 
                     <div className="nav_dropdown">
                         
                         <button className="dropbtn"><i className="fa fa-bars" aria-label="menu"></i></button>
                         <div className="dropdown-content">
-                            <a href="topics.html"><i className="fa fa-lightbulb-o"> Topics</i></a>
-                            <a href="survey.html"><i className="fa fa-search"> Survey</i></a>
-                            <a href="lifestyle.html"><i className="fa fa-calendar"> Lifestyle</i></a>
-                            <a href="books.html"><i className="fa fa-book"> Books</i></a>
-                            <a href="aboutus.html"><i className="fa fa-users"> About Us</i></a>
+                            <a href="#/" onClick={handleClick} name="topic"><i className="fa fa-lightbulb-o"> Topics</i></a>
+                            <a href="#/" onClick={handleClick} name="survey"><i className="fa fa-search"> Survey</i></a>
+                            <a href="#/" onClick={handleClick} name="lifestyle"><i className="fa fa-calendar"> Lifestyle</i></a>
+                            <a href="#/" onClick={handleClick} name="book"><i className="fa fa-book"> Books</i></a>
+                            <a href="#/" onClick={handleClick} name="aboutus"><i className="fa fa-users"> About Us</i></a>
                         </div>
         
                     </div>
