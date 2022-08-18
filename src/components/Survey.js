13da4bb83aef3ae0bb5f463ac9f705ba2f406c80
leo5302 <47090484+leo5302@users.noreply.github.com>
2022-08-18 01:21:06 -0700

    Survey swap to a different source of test

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,49 +1,65 @@
 import React, { useState }from 'react';
+import _ from 'lodash'
 
 export default function Survey(props) {
-    const [count, setCount] = useState({});
-
-    //Need to work on commented out area next time! I found a better self-assessment resources!
+    //https://www.psychiatry.org/psychiatrists/practice/dsm/educational-resources/assessment-measures#section_0
 
     const [range, setRange] = useState({});
-    const [displayText, setText] = useState({'range1': 'None', 'range2': 'None', 'range3': 'None', 'range4': 'None', 'range5': 'None'});
-
-    const handleCheck = (event) => {
-        let id = event.target.id;
-        setCount({...count, [id]: event.target.checked});
-    }
-
+    const [displayText, setText] = useState({'range1': 'None', 'range2': 'None', 'range3': 'None', 'range4': 'None', 'range5': 'None', 'range6': 'None', 'range7': 'None'});
     let rangeText = {1: 'Never', 2:'Rarely', 3:'Sometimes', 4:'Often', 5:'Always'};
-    //let displayText = {'range1': 'None', 'range2': 'None', 'range3': 'None', 'range4': 'None', 'range5': 'None'};
-    let range1Text = displayText.range1;
 
     const handleRange = (event) => {
         let id = event.target.id;
         setRange({...range, [id]: event.target.value});
         setText({...displayText, [id]: rangeText[event.target.value]});
-        
-        //displayText[id] = rangeText[event.target.value];
-
-        console.log(range);
-        console.log(displayText);
-
     }
+
     let result;
     const [resultText, setResult] = useState(result);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        let counts = 0;
-        let countArray = Object.values(count);
-        countArray.forEach((value) => {
-            if (value === true) counts++;
-        });
-        
-        if (counts >= 3) {
-            //Change page display
+        if (_.isEmpty(range)) {
             result = (
-                <div className='pt-3'>
-                    <p>You might need to contact a doctor!</p>
+                <div className='mt-3 alert alert-warning'>
+                    <p>Please answer each question by draging the slider!</p>
+                </div>
+            );
+            setResult(result);
+            return;
+        }
+
+        let counts = Object.values(range).reduce((a, b) => ~~a + ~~b);
+        console.log(counts);
+        
+        if (_.size(range) < 7) {
+            result = (
+                <div className='mt-3 alert alert-warning'>
+                    <p>Invalid Test, please make sure you have answered all the questions!</p>
+                </div>
+            );
+        } else if (counts >= 7 && counts < 16) {
+            result = (
+                <div className='mt-3 alert alert-success'>
+                    <p>You might have a <span className='font-weight-bold'>None to Slight</span> level of Anxiety</p>
+                </div>
+            );
+        } else if (counts >= 16 && counts <20) {
+            result = (
+                <div className='mt-3 alert alert-success'>
+                    <p>You might have a <span className='font-weight-bold'>Mild</span> level of Anxiety</p>
+                </div>
+            );
+        } else if (counts >= 20 && counts <28) {
+            result = (
+                <div className='mt-3 alert alert-success'>
+                    <p>You might have a <span className='font-weight-bold'>Moderate</span> level of Anxiety</p>
+                </div>
+            );
+        } else if (counts >= 28) {
+            result = (
+                <div className='mt-3 alert alert-success'>
+                    <p>You might have a <span className='font-weight-bold'>Severe</span> level of Anxiety</p>
                 </div>
             );
         } else {
@@ -51,9 +67,9 @@ export default function Survey(props) {
         }
 
         setResult(result);
+    }
 
-        // console.log(counts);
-        // console.log(result);
+    function sortTable(n) {
     }
 
     return (
@@ -61,6 +77,34 @@ export default function Survey(props) {
             <section className="survey">
                 <h2>Quick Mental Health Survey</h2>
                 <p className="text-justify">In life, things happen. Helping yourself is actually not that hard! It only takes 3 minutes to complete one survey. Knowing yourself is the first step to recovery!</p>
+
+                <table className="table">
+                    <thead className="thead-dark">
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col" onClick={sortTable}>Name</th>
+                        <th scope="col" onClick={sortTable}>Popularity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Anxiety</td>
+                        <td>123</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Depression</td>
+                        <td>1234</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td>Anger</td>
+                        <td>12345</td>
+                        </tr>
+                    </tbody>
+                </table>
+
                 <div className ="text-center m-3">
                     <h1 className="text-primary">Anxiety</h1>
                 </div>
@@ -86,47 +130,33 @@ export default function Survey(props) {
                     <h4 className="text-center p-3">In the past <span className='text-success'>7 days</span>, have you experienced any of the following condtions?</h4>
                     <div className="card border-dark">
                         <div className="card-body">
-                            <div className="form-check">
-                                <input className="form-check-input" type="checkbox" id="check1" onChange={handleCheck}/>
-                                <label className="form-check-label" htmlFor="check1">
-                                    Excessive anxiety and worry (apprehensive expectation), occurring more days than not for at least 6 months, about a number of events or activities (such as work or school performance).
-                                </label>
+                            <div className="form-group p-3">
+                                <label htmlFor="range1" className='font-weight-bold lead'>I felt fearful: <span className='border border-info rounded text-danger p-1 ml-3'>{displayText.range1}</span></label>
+                                <input type="range" className="custom-range" id="range1" min={1} max={5} onChange={handleRange}/>
                             </div>
-                            <div className="form-check">
-                                <input className="form-check-input" type="checkbox" id="check2" onChange={handleCheck}/>
-                                <label className="form-check-label" htmlFor="check2">
-                                    The individual finds it difficult to control the worry.
-                                </label>
+                            <div className="form-group p-3">
+                                <label htmlFor="range2" className='font-weight-bold lead'>I felt anxious: <span className='border border-info rounded text-danger p-1 ml-3'>{displayText.range2}</span></label>
+                                <input type="range" className="custom-range" id="range2" min={1} max={5} onChange={handleRange}/>
                             </div>
-                            <div className="form-check">
-                                <input className="form-check-input" type="checkbox" id="check3" onChange={handleCheck}/>
-                                <label className="form-check-label" htmlFor="check3">
-                                    The anxiety and worry are associated with three (or more) of the following six symptoms (with at least some symptoms having been present for more days than not for the past 6 months):
-                                </label>
-                                <ol>
-                                    <li className="m-1">Restlessness or feeling keyed up or on edge.</li>
-                                    <li className="m-1">Being easily fatigued.</li>
-                                    <li className="m-1">Difficulty concentrating or mind going blank.</li>
-                                    <li className="m-1">Irritability.</li>
-                                    <li className="m-1">Muscle tension.</li>
-                                    <li className="m-1">Sleep disturbance (difficulty falling or staying asleep, or restless, unsatisfying sleep).</li>
-                                </ol>
+                            <div className="form-group p-3">
+                                <label htmlFor="range3" className='font-weight-bold lead'>I felt worried: <span className='border border-info rounded text-danger p-1 ml-3'>{displayText.range3}</span></label>
+                                <input type="range" className="custom-range" id="range3" min={1} max={5} onChange={handleRange}/>
                             </div>
-                            <div className="form-check">
-                                <input className="form-check-input" type="checkbox" id="check4" onChange={handleCheck}/>
-                                <label className="form-check-label" htmlFor="check4">
-                                    The anxiety, worry, or physical symptoms cause clinically significant distress or impairment in social, occupational, or other important areas of functioning.
-                                </label>
+                            <div className="form-group p-3">
+                                <label htmlFor="range4" className='font-weight-bold lead'>I found it hard to focus on anything other than my anxiety: <span className='border border-info rounded text-danger p-1 ml-3'>{displayText.range4}</span></label>
+                                <input type="range" className="custom-range" id="range4" min={1} max={5} onChange={handleRange}/>
                             </div>
-                            <div className="form-check">
-                                <input className="form-check-input" type="checkbox" id="check5" onChange={handleCheck}/>
-                                <label className="form-check-label" htmlFor="check5">
-                                    The disturbance is not better explained by another mental disorder
-                                </label>
+                            <div className="form-group p-3">
+                                <label htmlFor="range5" className='font-weight-bold lead'>I felt nervous: <span className='border border-info rounded text-danger p-1 ml-3'>{displayText.range5}</span></label>
+                                <input type="range" className="custom-range" id="range5" min={1} max={5} onChange={handleRange}/>
                             </div>
-                            <div className="form-group">
-                                <label htmlFor="range1" className='font-weight-bold'>I felt fearful: <span className='pr-3'>{range1Text}</span></label>
-                                <input type="range" className="form-control-range" id="range1" min={1} max={5} onChange={handleRange}/>
+                            <div className="form-group p-3">
+                                <label htmlFor="range6" className='font-weight-bold lead'>I felt uneasy: <span className='border border-info rounded text-danger p-1 ml-3'>{displayText.range6}</span></label>
+                                <input type="range" className="custom-range" id="range6" min={1} max={5} onChange={handleRange}/>
+                            </div>
+                            <div className="form-group p-3">
+                                <label htmlFor="range7" className='font-weight-bold lead'>I felt tense: <span className='border border-info rounded text-danger p-1 ml-3'>{displayText.range7}</span></label>
+                                <input type="range" className="custom-range" id="range7" min={1} max={5} onChange={handleRange}/>
                             </div>
                         </div>
                     </div>
